81f287a5e5983054ea79fa2680d81fc54472d34e
sushma1802 <105391446+sushma1802@users.noreply.github.com>
2022-05-16 07:57:52 +0200

    Update qpsk_mod_and_demod.m

--- qpsk_mod_and_demod.m
+++ qpsk_mod_and_demod.m
@@ -1,22 +1,26 @@
+% generating carrier signals
+
 tb=1;
 t=0:tb/100:tb;
 f=1;
 c1= sqrt(2/tb)*cos(2*pi*f*t);
-c2= sqrt(2/tb)*sin(2*pi*f*t);
-subplot(3,2,1);
+c2= sqrt(2/tb)*sin(2*pi*f*t); 
+subplot(3,2,1);    % plotting carrier signals
 plot(t,c1);
 subplot(3,2,2);
 plot(t,c2);
 grid on;
 
-
+% generating message signal
 N=16;
 m=rand(1,N);
 t1=0;
 t2=tb;
-subplot(3,2,3);
+subplot(3,2,3);    % plotting message signal
 stem(m);
 grid on;
+
+% qpsk modulating code
 
 for i=1:2:(N-1)
 if m(i)>0.5
@@ -39,10 +43,13 @@ evensig(i,:)=c1 .* ms;
 
 qpsk=oddsig + evensig;
 
-subplot(3,2,4);
+subplot(3,2,4); % plotting qpsk modulated signal   
 plot(t,qpsk);
 grid on;
 hold on;
+
+% qpsk demodulating code
+
 t1= t1+(tb+0.01);
 t2= t2+(tb+0.01);
 end
@@ -67,6 +74,6 @@ demod(i)=0;
 demod(i+1)=0;
 end
 end
-subplot(3,2,5);
+subplot(3,2,5);    % plotting demodulated signal
 stem(demod);
 grid on;
